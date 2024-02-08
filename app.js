ebe644363dadd49339efa20e6c5da3839d6a249a
ErickAriasC <eariascardona0@soy.sena.edu.co>
2024-02-08 11:30:48 -0500

    aplicacion de la api con actualizacion de card

---
+++ app.js
@@ -0,0 +1,29 @@
+const container = document.querySelector('main')
+
+const btnGetCard = document.querySelector('.get_card')
+
+const URL = 'https://thesimpsonsquoteapi.glitch.me/quotes'
+
+function getAPI () {
+    fetch(URL)
+    .then(response => response.json())
+    .then(data => getCard(data[0]))
+    .catch(err => console.log(err));
+}
+
+function getCard(Simpson) {
+     const {quote, image, character} = Simpson; 
+
+     const imgCard = document.querySelector('#img')
+     imgCard.src = image
+     imgCard.alt = character
+
+     const nameCard = document.querySelector('.nameCharacter')
+     nameCard.textContent = character
+
+     const quoteCard = document.querySelector('.description')
+     quoteCard.textContent = quote
+
+}
+
+btnGetCard.addEventListener('click', getAPI)
